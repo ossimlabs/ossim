223f4c936a6d6fe25b4bcf7387f074499be8902b
oscarkramer <okramer@radiantblue.com>
2015-09-18 12:50:51 -0400

    Testing something

--- scripts/linux/test.sh
+++ scripts/linux/test.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
+###############################################################################
+#
+# Test script for all OSSIM repositories
+#
+# This script must be run from the top "ossimlabs" directory containing ossim, 
+# and other OSSIM-related repositories. It performs three functions:
+# 
+# No env vars need to be predefined. 
+#
+###############################################################################
+
 # TEST 1: Check ossim-info version:
-COUNT=`build/bin/ossim-info --version | grep --count "ossim-info 1.9"`
+COUNT=`/var/lib/go-agent/pipelines/ossimlabs-pipeline/build/bin/ossim-info --version | grep --count "ossim-info 1.9"`
 if [ $COUNT != "1" ]; then
   echo "Failed TEST 1"; exit 1
 fi
