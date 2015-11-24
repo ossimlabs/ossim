71c80e7dcd0b7a0dacfc66d849674cb317453343
oscarkramer <okramer@radiantblue.com>
2015-11-24 10:32:47 -0500

    Fix script for rsync and add prefs file

--- scripts/linux/setup.sh
+++ scripts/linux/setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 ###############################################################################
 #
 # Setup script for all OSSIM repositories
@@ -13,62 +13,65 @@
 ###############################################################################
 
 if [ -z $OSSIM_DATA ] || [ ! -d $OSSIM_DATA ] ; then
-  mkdir -p /data/ossim_data
-  export OSSIM_DATA=/data/ossim_data
-if
+  mkdir -p /data/ossim_data;
+  export OSSIM_DATA=/data/ossim_data;
+fi
 
 if [ -z $USING_S3SYNC ] ; then
-  echo; echo "STATUS: Syncing data directory to S3..."
-  s3cmd sync --no-check-md5 s3://yumrepos-dev-rbtcloud/ossim_data/public $OSSIM_DATA/
+  echo; echo "STATUS: Syncing data directory to S3...";
+  s3cmd sync --no-check-md5 s3://yumrepos-dev-rbtcloud/ossim_data/public $OSSIM_DATA/;
   if [ $? != 0 ]; then
-    echo "ERROR: Failed S3 sync."
-    exit 1
+    echo "ERROR: Failed S3 sync.";
+    exit 1;
   fi
-else
-  echo; echo "STATUS: Syncing data directory to data repository..."
-  if [ ! -z $OSSIM_DATA_REPOSITORY ] ; then
-    echo "ERROR: Env var OSSIM_DATA_REPOSITORY must be defined in order to syncronize against data repository."
-    exit 1
-  fi
-  if [ ! -d $OSSIM_DATA_REPOSITORY ] ; then
-    echo "ERROR: $OSSIM_DATA_REPOSITORY is not a valid directory/mount point."
-    exit 1
-  fi
-  
-  # rsync elevation data:
-  rsync -rm --delete $OSSIM_DATA_REPOSITORY/elevation/dted/level0 $OSSIM_DATA/elevation/dted
-  if [ $? != 0 ]; then 
-    echo "ERROR: Failed data repository rsync of elevation."
-    exit 1
-  fi
-  
-  # rsync nadcon data:
-  rsync -rm --delete $OSSIM_DATA_REPOSITORY/elevation/nadcon $OSSIM_DATA/elevation
-  if [ $? != 0 ]; then 
-    echo "ERROR: Failed data repository rsync of nadcon grids."
-    exit 1
-  fi
-  
-  # rsync geoid 96 data:
-  rsync -rm --delete $OSSIM_DATA_REPOSITORY/elevation/geoid96_little_endian/ $OSSIM_DATA/elevation/geoids/geoid96
-  if [ $? != 0 ]; then 
-    echo "ERROR: Failed data repository rsync of geoid96 grids."
-    exit 1
-  fi
-  
-  # rsync geoid 99 data:
-  rsync -rm --delete $OSSIM_DATA_REPOSITORY/elevation/geoid99_little_endian/ $OSSIM_DATA/elevation/geoids/geoid99
-  if [ $? != 0 ]; then 
-    echo "ERROR: Failed data repository rsync of geoid99 grids."
-    exit 1
-  fi
-  
-  #rsync imagery
-  rsync -rm --delete $OSSIM_DATA_REPOSITORY/test/data/public/ $OSSIM_DATA/ossim_data
-  if [ $? != 0 ]; then 
-    echo "ERROR: Failed data repository rsync of imagery."
-    exit 1
-  fi
+  exit 0;
 fi
-   
-exit 0
+
+echo; echo "STATUS: Syncing data directory to data repository...";
+if [ ! -z $OSSIM_DATA_REPOSITORY ] ; then
+  echo "ERROR: Env var OSSIM_DATA_REPOSITORY must be defined in order to syncronize against data repository.";
+  exit 1;
+fi
+if [ ! -d $OSSIM_DATA_REPOSITORY ] ; then
+  echo "ERROR: $OSSIM_DATA_REPOSITORY is not a valid directory/mount point.";
+  exit 1;
+fi
+
+# rsync elevation data:
+rsync -rm --delete $OSSIM_DATA_REPOSITORY/elevation/dted/level0 $OSSIM_DATA/elevation/dted;
+if [ $? != 0 ] ; then 
+  echo "ERROR: Failed data repository rsync of elevation.";
+  exit 1;
+fi
+
+# rsync nadcon data:
+rsync -rm --delete $OSSIM_DATA_REPOSITORY/elevation/nadcon $OSSIM_DATA/elevation;
+if [ $? != 0 ] ; then 
+  echo "ERROR: Failed data repository rsync of nadcon grids.";
+  exit 1;
+fi
+
+# rsync geoid 96 data:
+rsync -rm --delete $OSSIM_DATA_REPOSITORY/elevation/geoid96_little_endian/ $OSSIM_DATA/elevation/geoids/geoid96;
+if [ $? != 0 ] ; then 
+  echo "ERROR: Failed data repository rsync of geoid96 grids.";
+  exit 1;
+fi
+
+# rsync geoid 99 data:
+rsync -rm --delete $OSSIM_DATA_REPOSITORY/elevation/geoid99_little_endian/ $OSSIM_DATA/elevation/geoids/geoid99;
+if [ $? != 0 ] ; then 
+  echo "ERROR: Failed data repository rsync of geoid99 grids.";
+  exit 1;
+fi
+
+#rsync imagery
+rsync -rm --delete $OSSIM_DATA_REPOSITORY/test/data/public/ $OSSIM_DATA/ossim_data;
+if [ $? != 0 ] ; then 
+  echo "ERROR: Failed data repository rsync of imagery.";
+  exit 1;
+fi
+  
+exit 0;
+
+
