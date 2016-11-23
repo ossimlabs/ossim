a44f66f2755e0b8b90a60f0c73da923302498d7d
David Burken <dburken@comcast.net>
2016-11-23 09:30:19 -0500

    Added stubs to set paths for non-standard installs.

--- support/linux/etc/profile.d/ossim.sh
+++ support/linux/etc/profile.d/ossim.sh
@@ -9,3 +9,12 @@ fi
 if [ -z "$OSSIM_PREFS_FILE" ]; then
    export OSSIM_PREFS_FILE=$OSSIM_INSTALL_PREFIX/share/ossim/ossim-site-preferences
 fi
+
+#---
+# Stub to set path for non-standard install:
+#---
+# ossim_bin=/usr/local/ossim/bin
+# if ! echo ${PATH} | /bin/grep -q $ossim_bin ; then
+#    PATH=$ossim_bin:${PATH}
+# fi
+# export PATH
