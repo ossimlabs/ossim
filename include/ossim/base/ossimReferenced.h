c374813500edbb72c2c89ddc86d2e546d949cdb7
Garrett Potts <gpotts@radiantblue.com>
2017-09-18 14:29:39 -0400

    We are now using the std::atomic for ref counting and removed the std:mutex lock

--- include/ossim/base/ossimReferenced.h
+++ include/ossim/base/ossimReferenced.h
@@ -6,72 +6,92 @@
 #define ossimReferenced_HEADER
 #include <ossim/base/ossimConstants.h>
 
-#include <mutex>
+#include <atomic>
 
+/**
+* ossimReferenced allows for shared object ref counting
+* if the reference count ever gets to 0 or less it will
+* delete 'this' object.
+*
+* Currently uses std::mutex to control the locking of the reference
+* count variable.
+*
+* Eventually we would like to replace all of ossimReferenced and ossimRefPtr
+* with C++11 std::shared_ptr
+*/
 class OSSIMDLLEXPORT ossimReferenced
 {
  public:
    ossimReferenced()
-   : theRefCount(0)
+   : m_refCount(0)
       {}
    
    ossimReferenced(const ossimReferenced&)
-   : theRefCount(0)
+   : m_refCount(0)
    {}
    inline ossimReferenced& operator = (const ossimReferenced&) { return *this; }
    
 
-   /*! increment the reference count by one, indicating that 
-       this object has another pointer which is referencing it.*/
+   /** 
+    * increment the reference count by one, indicating that 
+    * this object has another pointer which is referencing it.
+    */
    inline void ref() const;
    
-   /*! decrement the reference count by one, indicating that 
-       a pointer to this object is referencing it.  If the
-       reference count goes to zero, it is assumed that this object
-       is no longer referenced and is automatically deleted.*/
+   /**
+    * decrement the reference count by one, indicating that 
+    * a pointer to this object is referencing it.  If the
+    * reference count goes to zero, it is assumed that this object
+    * is no longer referenced and is automatically deleted.
+    */
    inline void unref() const;
    
-   /*! decrement the reference count by one, indicating that 
-       a pointer to this object is referencing it.  However, do
-       not delete it, even if ref count goes to 0.  Warning, unref_nodelete() 
-       should only be called if the user knows exactly who will
-       be resonsible for, one should prefer unref() over unref_nodelete() 
-       as the later can lead to memory leaks.*/
+   /**
+    * decrement the reference count by one, indicating that 
+    * a pointer to this object is referencing it.  However, do
+    * not delete it, even if ref count goes to 0.  Warning, unref_nodelete() 
+    * should only be called if the user knows exactly who will
+    * be resonsible for, one should prefer unref() over unref_nodelete() 
+    * as the later can lead to memory leaks.
+    */
    inline void unref_nodelete() const 
    { 
-     std::lock_guard<std::mutex> lock(theRefMutex); 
-     --theRefCount;
+     // std::lock_guard<std::mutex> lock(theRefMutex); 
+     --m_refCount;
    }
    
-   /*! return the number pointers currently referencing this object. */
-   inline int referenceCount() const { return theRefCount; }
+   /**
+    * @return the number pointers currently referencing this object. 
+    */
+   inline int referenceCount() const { return m_refCount.load(); }
    
    
  protected:
    virtual ~ossimReferenced();
-   mutable std::mutex theRefMutex;
-   mutable int        theRefCount;
+
+ private:
+    mutable std::atomic_int      m_refCount;
 };
 
 inline void ossimReferenced::ref() const
 {
-  std::lock_guard<std::mutex> lock(theRefMutex); 
-  ++theRefCount;
+  ++m_refCount;
 }
 
 inline void ossimReferenced::unref() const
 {
-   bool needDelete = false;
-   {
-      std::lock_guard<std::mutex> lock(theRefMutex); 
-      --theRefCount;
-      needDelete = theRefCount<=0;
-   }
-   
-   if (needDelete)
-   {
-      delete this;
-   }
+  bool needDelete = false;
+  {
+    // fetch_sub should return the value before subtraction.
+    // so if 1 was before the subtraction that means it should
+    // be zero after the subtraction so we will test <=1
+    needDelete = m_refCount.fetch_sub(1) <= 1;
+  }
+
+  if (needDelete)
+  {
+    delete this;
+  }
 }
 
 #endif
--- src/base/ossimReferenced.cpp
+++ src/base/ossimReferenced.cpp
@@ -5,10 +5,11 @@
 
 ossimReferenced::~ossimReferenced()
 {
-   if (theRefCount>0)
+   int count = referenceCount();
+   if (count>0)
    {
       ossimNotify(ossimNotifyLevel_WARN)<<"Warning: deleting still referenced object "<<this<<std::endl;
-      ossimNotify(ossimNotifyLevel_WARN)<<"         the final reference count was "<<theRefCount
+      ossimNotify(ossimNotifyLevel_WARN)<<"         the final reference count was "<<count
                                         <<", memory corruption possible."<<std::endl;
    }
 }
