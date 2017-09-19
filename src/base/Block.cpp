f2829e3ddf426f5c1c076a496c1248679c65f81c
Garrett Potts <gpotts@radiantblue.com>
2017-09-19 11:41:48 -0400

    Added documenation and add guarantee that all threads are released when returning from release

--- src/base/Block.cpp
+++ src/base/Block.cpp
@@ -1,7 +1,9 @@
 #include <ossim/base/Block.h>
+#include <iostream>
 
 ossim::Block::Block(bool releaseFlag)
-:m_release(releaseFlag)
+:m_release(releaseFlag),
+m_waitCount(0)
 {
 
 }
@@ -26,11 +28,15 @@ void ossim::Block::block()
    std::unique_lock<std::mutex> lock(m_mutex);
    if(!m_release)
    {
+      ++m_waitCount;
       m_conditionVariable.wait(lock, [this]{
          return (m_release.load() == true);
       });
+      --m_waitCount;
+      if(m_waitCount < 0) m_waitCount = 0;
    }
    m_conditionVariable.notify_all();   
+   m_conditionalWait.notify_all();
 }
 
 void ossim::Block::block(ossim_uint64 waitTimeMillis)
@@ -38,15 +44,18 @@ void ossim::Block::block(ossim_uint64 waitTimeMillis)
    std::unique_lock<std::mutex> lock(m_mutex);
    if(!m_release)
    {
+      ++m_waitCount;
       m_conditionVariable.wait_for(lock, 
                                    std::chrono::milliseconds(waitTimeMillis),
                                    [this]{
          return (m_release.load() == true);
       });
+      --m_waitCount;
+      if(m_waitCount < 0) m_waitCount = 0;
    }
    m_conditionVariable.notify_all();   
+   m_conditionalWait.notify_all();
 }
-
 void ossim::Block::release()
 {
    {   
@@ -55,13 +64,18 @@ void ossim::Block::release()
       {
          m_release = true;
       }
+      m_conditionVariable.notify_all();
+      if(m_waitCount>0)
+      {
+         m_conditionalWait.wait(lock, [this]{return m_waitCount.load()<1;});
+      }
    }
-   m_conditionVariable.notify_all();
 }
 
 void ossim::Block::reset()
 {
    std::unique_lock<std::mutex> lock(m_mutex);
 
-   m_release = false;
+   m_release   = false;
+   m_waitCount = 0;
 }
