8e03445c6a36d648f7bf18b8f73be6efdc6f8b9b
Garrett Potts <gpotts@radiantblue.com>
2017-09-22 17:07:51 -0400

    Added clear in the destructor

--- src/parallel/ossimJobMultiThreadQueue.cpp
+++ src/parallel/ossimJobMultiThreadQueue.cpp
@@ -11,6 +11,8 @@ ossimJobMultiThreadQueue::~ossimJobMultiThreadQueue()
 {
    cancel();
    waitForCompletion();
+   m_threadQueueList.clear();
+   m_jobQueue = nullptr;
 }
 
 std::shared_ptr<ossimJobQueue> ossimJobMultiThreadQueue::getJobQueue()
