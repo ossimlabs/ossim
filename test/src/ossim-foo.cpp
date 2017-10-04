07fc0128849de74de18207910656785ed1b9eef6
oscarkramer <okramer@radiantblue.com>
2017-10-04 11:57:48 -0400

    Modified to use shared_ptr-like bool mthods

--- test/src/ossim-foo.cpp
+++ test/src/ossim-foo.cpp
@@ -1,119 +1,63 @@
-//---
-//
-// License: MIT
-//
-// File: ossim-foo.cpp
-//
-// Description: Contains application definition "ossim-foo" app.
-//
-// NOTE:  This is supplied for simple quick test. DO NOT checkin your test to
-//        the svn repository.  Simply edit ossim-foo.cpp and run your test.
-//        After completion you can do a "git checkout -- ossimfoo.cpp" if
-//        you want to keep your working repository up to snuff.
-//
-// $Id$
-//---
-
-// ossim includes:  These are here just to save time/typing...
-#include <ossim/base/ossimApplicationUsage.h>
-#include <ossim/base/ossimArgumentParser.h>
-#include <ossim/base/ossimConnectableObject.h>
-#include <ossim/base/ossimCsvFile.h>
-#include <ossim/base/ossimDate.h>
-#include <ossim/base/ossimDpt.h>
-#include <ossim/base/ossimDrect.h>
-#include <ossim/base/ossimException.h>
-#include <ossim/base/ossimFilename.h>
-#include <ossim/base/ossimIpt.h>
-#include <ossim/base/ossimIrect.h>
-#include <ossim/base/ossimKeywordlist.h>
-#include <ossim/base/ossimKeywordNames.h>
-#include <ossim/base/ossimNotify.h>
-#include <ossim/base/ossimObjectFactory.h>
-#include <ossim/base/ossimObjectFactoryRegistry.h>
-#include <ossim/base/ossimProperty.h>
-#include <ossim/base/ossimRefPtr.h>
-#include <ossim/base/ossimString.h>
-#include <ossim/base/ossimScalarTypeLut.h>
-#include <ossim/base/ossimStdOutProgress.h>
-#include <ossim/base/ossimStreamFactoryRegistry.h>
-#include <ossim/base/ossimStringProperty.h>
-#include <ossim/base/ossimTrace.h>
-#include <ossim/base/ossimUrl.h>
-#include <ossim/base/ossimVisitor.h>
-#include <ossim/base/ossimEcefPoint.h>
-#include <ossim/base/ossimEcefVector.h>
-#include <ossim/base/ossim2dBilinearTransform.h>
-
-#include <ossim/imaging/ossimNitfTileSource.h>
-#include <ossim/imaging/ossimBrightnessContrastSource.h>
-#include <ossim/imaging/ossimBumpShadeTileSource.h>
-#include <ossim/imaging/ossimFilterResampler.h>
-#include <ossim/imaging/ossimFusionCombiner.h>
-#include <ossim/imaging/ossimImageData.h>
-#include <ossim/imaging/ossimImageFileWriter.h>
-#include <ossim/imaging/ossimImageGeometry.h>
-#include <ossim/imaging/ossimImageHandler.h>
-#include <ossim/imaging/ossimImageMosaic.h>
-#include <ossim/imaging/ossimImageRenderer.h>
-#include <ossim/imaging/ossimImageSource.h>
-#include <ossim/imaging/ossimImageSourceFilter.h>
-#include <ossim/imaging/ossimImageToPlaneNormalFilter.h>
-#include <ossim/imaging/ossimImageWriterFactoryRegistry.h>
-#include <ossim/imaging/ossimIndexToRgbLutFilter.h>
-#include <ossim/imaging/ossimRectangleCutFilter.h>
-#include <ossim/imaging/ossimScalarRemapper.h>
-#include <ossim/imaging/ossimSFIMFusion.h>
-#include <ossim/imaging/ossimTwoColorView.h>
-#include <ossim/imaging/ossimImageSourceFactoryRegistry.h>
-#include <ossim/imaging/ossimImageHandlerRegistry.h>
-
-#include <ossim/init/ossimInit.h>
-
-#include <ossim/projection/ossimEquDistCylProjection.h>
-#include <ossim/projection/ossimImageViewAffineTransform.h>
-#include <ossim/projection/ossimMapProjection.h>
-#include <ossim/projection/ossimProjection.h>
-#include <ossim/projection/ossimProjectionFactoryRegistry.h>
-#include <ossim/projection/ossimUtmProjection.h>
-
-#include <ossim/support_data/ossimSrcRecord.h>
-#include <ossim/support_data/ossimWkt.h>
-
-#include <ossim/base/Barrier.h>
-#include <ossim/base/Block.h>
-#include <ossim/base/Thread.h>
-
-// Put your includes here:
-
-// System includes:
-#include <cmath>
-#include <memory>
-#include <sstream>
 #include <iostream>
-int main(int argc, char *argv[])
-{
-   int returnCode = 0;
-   
-   ossimArgumentParser ap(&argc, argv);
-   ossimInit::instance()->addOptions(ap);
-   ossimInit::instance()->initialize(ap);
+#include <ossim/base/ossimRefPtr.h>
+#include <ossim/base/ossimReferenced.h>
 
-   try
-   {
-      // Put your code here.
-   }
-   catch(const ossimException& e)
-   {
-      ossimNotify(ossimNotifyLevel_WARN) << e.what() << std::endl;
-      returnCode = 1;
-   }
-   catch( ... )
-   {
-      ossimNotify(ossimNotifyLevel_WARN)
-         << "ossim-foo caught unhandled exception!" << std::endl;
-      returnCode = 1;
-   }
-   
-   return returnCode;
+using namespace std;
+
+class TestClass : public ossimReferenced
+{
+public:
+   TestClass() {}
+};
+
+int main()
+{
+   cout << "Hello World!" << endl; // prints Hello World!
+
+   ossimRefPtr<TestClass> p0;
+   ossimRefPtr<TestClass> p1 = new TestClass;
+   ossimRefPtr<TestClass> p1c = p1;
+   ossimRefPtr<TestClass> p2 = new TestClass;
+
+   if (p0)
+      cout<<"(p0) TRUE"<<endl;
+   else
+      cout<<"(p0) FALSE"<<endl;
+
+   if (!p0)
+      cout<<"(!p0) TRUE"<<endl;
+   else
+      cout<<"(!p0) FALSE"<<endl;
+
+   if (p1)
+      cout<<"(p1) TRUE"<<endl;
+   else
+      cout<<"(p1) FALSE"<<endl;
+
+   if (!p1)
+      cout<<"(!p1) TRUE"<<endl;
+   else
+      cout<<"(!p1) FALSE"<<endl;
+
+   if (p1 == p2)
+      cout<<"(p1 == p2) TRUE"<<endl;
+   else
+      cout<<"(p1 == p2) FALSE"<<endl;
+
+   if (p1 != p2)
+      cout<<"(p1 != p2) TRUE"<<endl;
+   else
+      cout<<"(p1 != p2) FALSE"<<endl;
+
+   if (p1 == p1c)
+      cout<<"(p1 == p1c) TRUE"<<endl;
+   else
+      cout<<"(p1 == p1c) FALSE"<<endl;
+
+   if (p1 != p1c)
+      cout<<"(p1 != p1c) TRUE"<<endl;
+   else
+      cout<<"(p1 != p1c) FALSE"<<endl;
+
+   return 0;
 }
