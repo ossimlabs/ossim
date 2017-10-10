1bfa12eb578e6d62325decbb4633999d4c529a4f
Garrett Potts <gpotts@radiantblue.com>
2017-10-10 09:45:14 -0400

    Added state information for valid vertices, ... etc

--- include/ossim/support_data/ImageHandlerState.h
+++ include/ossim/support_data/ImageHandlerState.h
@@ -8,6 +8,7 @@
 #include <ossim/base/State.h>
 #include <ossim/base/ossimIpt.h>
 #include <ossim/base/ossimFilename.h>
+#include <ossim/support_data/ossimImageMetaData.h>
 
 namespace ossim
 {
@@ -29,18 +30,23 @@ namespace ossim
       const ossimString& getConnectionString()const{return m_connectionString;}
       void setImageHandlerType(const ossimString& typeName){m_imageHandlerType = typeName;}
       const ossimString& getImageHandlerType()const{return m_imageHandlerType;}
-      void setCurrentEntry(const ossimString& entry){m_currentEntry = entry;}
-      const ossimString& getCurrentEntry()const{return m_currentEntry;}
-      // const ossimKeywordlist& getMetaData()const{return m_omd;}
-      // void setMetaData(const ossimKeywordlist& omd){m_omd=omd;}
-      // bool hasMetaData()const{return m_omd.getSize()>0;}
+      void setCurrentEntry(ossim_uint32 entry){m_currentEntry = entry;}
+      const ossim_uint32 getCurrentEntry()const{return m_currentEntry;}
+      std::shared_ptr<const ossimImageMetaData> getMetaData()const{return m_omd;}
+      std::shared_ptr<ossimImageMetaData> getMetaData(){return m_omd;}
+      void setMetaData(std::shared_ptr<ossimImageMetaData> omd){m_omd=omd;}
+      void setValidVertices(std::shared_ptr<ossimKeywordlist> kwl){m_validVertices=kwl;}
+      std::shared_ptr<ossimKeywordlist> getValidVertices(){return m_validVertices;}
+      std::shared_ptr<const ossimKeywordlist> getValidVertices()const{return m_validVertices;}
+      bool hasMetaData()const;
     private:
-      static const ossimString m_typeName;
-      std::shared_ptr<ImageHandlerState> m_overviewState;
-      ossimString                        m_connectionString;
-      ossimString                        m_imageHandlerType;
-      ossimString                        m_currentEntry;
-//      ossimKeywordlist                   m_omd;
+      static const ossimString            m_typeName;
+      std::shared_ptr<ImageHandlerState>  m_overviewState;
+      std::shared_ptr<ossimImageMetaData> m_omd;
+      std::shared_ptr<ossimKeywordlist>   m_validVertices;
+      ossimString                         m_connectionString;
+      ossimString                         m_imageHandlerType;
+      ossim_uint32                        m_currentEntry;
    };
 };
 
