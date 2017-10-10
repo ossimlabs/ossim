3657b456754e648493146694cad876532471f196
Garrett Potts <gpotts@radiantblue.com>
2017-10-10 09:52:40 -0400

    Added metadata and valid vertices support

--- src/support_data/ImageHandlerState.cpp
+++ src/support_data/ImageHandlerState.cpp
@@ -12,7 +12,7 @@ const ossimString ossim::ImageHandlerState::m_typeName = "ossim::ImageHandlerSta
 
 ossim::ImageHandlerState::ImageHandlerState()
 : State(),
-m_currentEntry("0")
+m_currentEntry(0)
 {
 
 }
@@ -32,6 +32,16 @@ const ossimString& ossim::ImageHandlerState::getStaticTypeName()
    return m_typeName;
 }
 
+bool ossim::ImageHandlerState::hasMetaData()const
+{
+  if(m_omd)
+  {
+      return m_omd->isValid(); 
+  }
+
+  return false;
+}
+
 void ossim::ImageHandlerState::load(const ossimKeywordlist& kwl,
                                     const ossimString& prefix)
 {
@@ -39,9 +49,31 @@ void ossim::ImageHandlerState::load(const ossimKeywordlist& kwl,
    m_overviewState = ossim::ImageHandlerStateRegistry::instance()->createState(kwl, prefix+"overview.");
    m_connectionString = kwl.find(prefix, "connection_string");
    m_imageHandlerType = kwl.find(prefix, "image_handler_type");
-   m_currentEntry     = kwl.find(prefix, "current_entry");
+   ossimString currentEntry = kwl.find(prefix, "current_entry");
 
-   if(m_currentEntry.empty()) m_currentEntry = "0";
+   if(currentEntry.empty())
+   {
+      m_currentEntry = 0;
+   } 
+   else
+   {
+      m_currentEntry = currentEntry.toUInt32();
+   }
+
+   ossimString omdType = kwl.find(prefix, "ossimImageMetaData");
+   if(!omdType.empty())
+   {
+      m_omd = std::make_shared<ossimImageMetaData>();
+      
+      m_omd->loadState(kwl, prefix+"omd.");
+   }
+   ossimKeywordlist tempKwl;
+   kwl.extractKeysThatMatch(tempKwl, "^("+prefix+"valid_vertices.)");
+   if(tempKwl.getSize())
+   {
+      tempKwl.stripPrefixFromAll("^("+prefix+"valid_vertices.)");
+      m_validVertices = std::make_shared<ossimKeywordlist>(tempKwl);
+   }
 }
 
 void ossim::ImageHandlerState::save(ossimKeywordlist& kwl,
@@ -53,7 +85,19 @@ void ossim::ImageHandlerState::save(ossimKeywordlist& kwl,
       ossimString tempPrefix = prefix + "overview.";
       m_overviewState->save(kwl, tempPrefix);
    }
-   kwl.add(prefix, "connection_string",  m_connectionString.c_str());
-   kwl.add(prefix, "image_handler_type", m_imageHandlerType.c_str());
-   kwl.add(prefix, "current_entry",      m_currentEntry.c_str());
+   if(m_omd)
+   {
+      ossimString tempPrefix = prefix + "omd.";
+      m_omd->saveState(kwl, tempPrefix);      
+   }
+   kwl.add(prefix, "connection_string",  m_connectionString.c_str(), true);
+   kwl.add(prefix, "image_handler_type", m_imageHandlerType.c_str(), true);
+   kwl.add(prefix, "current_entry",      m_currentEntry, true);
+
+   if(m_validVertices)
+   {
+      ossimString validVerticesPrefix = prefix+"valid_vertices.";
+      kwl.add(validVerticesPrefix.c_str(), *m_validVertices);
+      kwl.add(validVerticesPrefix, "type", "ossimValidVertices");
+   }
 }
