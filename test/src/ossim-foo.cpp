749ef691471b2aa97f9992f775981cd05f4b6de6
Garrett Potts <gpotts@radiantblue.com>
2017-11-21 08:12:30 -0500

    Added generic state cache

--- test/src/ossim-foo.cpp
+++ test/src/ossim-foo.cpp
@@ -68,6 +68,8 @@
 #include <ossim/imaging/ossimTwoColorView.h>
 #include <ossim/imaging/ossimImageSourceFactoryRegistry.h>
 #include <ossim/imaging/ossimImageHandlerRegistry.h>
+#include <ossim/support_data/ImageHandlerState.h>
+#include <ossim/imaging/ImageHandlerStateCache.h>
 
 #include <ossim/init/ossimInit.h>
 
@@ -94,6 +96,422 @@
 #include <sstream>
 #include <iostream>
 
+
+
+#if 0
+namespace ossim{
+   class JsonValue
+   {
+   public:
+   };
+
+   class JsonString : public JsonValue
+   {
+   public:
+   };
+
+   class JsonArray : public JsonValue
+   {
+   public:
+   };
+
+   class JsonObject : public JsonValue
+   {
+   public:
+      typedef std::map<ossimString, std::shared_ptr<JsonValue> > ObjectMap;
+
+      std::shared_ptr<const JsonValue> operator [] (const ossimString& key)const
+      {
+         return 0;
+      }
+      void add(const ossimString& key, std::shared_ptr<JsonValue> value)
+      {
+         m_map.insert(std::make_pair(key, value));
+      }
+
+   protected:
+      ObjectMap m_map;      
+   };
+
+   class Json
+   {
+   public:
+      bool parse(const ossimFilename& file);
+      bool parse(ossim::istream& in);
+      bool parse(const ossimString& buf);
+
+      bool parseObject(ossim::istream& in);
+      bool parseNumber(ossim::istream& in);
+      bool parseArray(ossim::istream& in);
+      bool parseValue(ossim::istream& in);
+      bool parseString(ossim::istream& in);
+      bool parseStringValue(ossim::istream& in);
+   protected:
+      void skipWhitespace(ossim::istream& in)const;
+      //std::map<std::string, std::shared_ptr<JsonValue> obj;
+   };
+   void Json::skipWhitespace(ossim::istream& in)const
+   {
+      int c = in.peek();
+      while( !in.fail() &&
+            ( (c == ' ') || (c == '\t') || (c == '\n') || (c == '\r') ) )
+      {
+         in.ignore(1);
+         c = in.peek();
+      }
+   }
+
+   bool Json::parse(ossim::istream& in)
+   {
+      return parseObject(in);
+   }
+   bool Json::parse(const ossimString& buf)
+   {
+      std::istringstream in(buf.c_str());
+
+      return parse(in);
+   }
+   bool Json::parseStringValue(ossim::istream& in)
+   {
+      skipWhitespace(in);
+      if(in.good())
+      {
+         if(parseString(in))
+         {
+            skipWhitespace(in);
+            if(in.good())
+            {
+               if(in.peek() == ':')
+               {
+                  std::cout << ":";
+                  in.ignore();
+                  if(parseValue(in))
+                  {
+
+                  }
+                  else
+                  {
+                     return false;
+                  }
+               }
+            }
+         }
+         else
+         {
+            return false;
+         }
+      }
+
+      return in.good();
+   }
+   bool Json::parseString(ossim::istream& in)
+   {
+      skipWhitespace(in);
+      if(in.peek() == '"')
+      {
+         std::cout << "\"";
+         in.ignore();
+         while(in.good())
+         {
+            int c = in.peek();
+            if(c == '\\')
+            {
+               in.ignore();
+               c = in.peek();
+               switch(c)
+               {
+                  case '"':
+                  {
+                     c = '"';
+                     break;
+                  }
+                  case 'b':
+                  {
+                     c = '\b';
+                     break;
+                  }
+                  case '\\':
+                  {
+                     c = '\\';
+                     break;
+                  }
+                  case '/':
+                  {
+                     c = '/';
+                     break;
+                  }
+                  case 'f':
+                  {
+                     c = '\f';
+                     break;
+                  }
+                  case 'n':
+                  {
+                     c = '\n';
+                     break;
+                  }
+                  case 'r':
+                  {
+                     c = '\r';
+                     break;
+                  }
+                  case 't':
+                  {
+                     c = '\t';
+                     break;
+                  }
+                  case 'u':
+                  {
+                     return false;
+                     break;
+                  }
+                  default:
+                  {
+                     return false;
+                  }
+               }
+            }
+            in.ignore();
+            std::cout << (char)c;
+            if(c == '"')
+            {
+               return true;
+            }
+         }
+      }
+      return false;
+   }
+   bool Json::parseNumber(ossim::istream& in)
+   {
+      bool done=false;
+      bool afterDecimal = false;
+      skipWhitespace(in);
+      int c = in.peek();
+      int sign = 1;
+      if(c == '-')
+      {
+         // starts with minus
+         std::cout << (char)c;
+         in.ignore();
+         c = in.peek();
+         sign = -1;
+      }
+      while(in.good() && !done)
+      {
+         if(std::isdigit(c))
+         {
+            std::cout << (char)c;
+            in.ignore();
+         }
+         else if(c == '.' &&!afterDecimal)
+         {
+            std::cout << (char)c;
+            in.ignore();
+            if(!std::isdigit(in.peek()))
+            {
+               return false;
+            }
+            afterDecimal = true;
+         }
+         else if(c == 'e' || c == 'E')
+         {
+            std::cout << (char)c;
+            in.ignore();
+            c = in.peek();
+            afterDecimal = true;
+            if(c == '+')
+            {
+               std::cout << (char)c;
+               in.ignore();
+            }
+            else if(c == '-')
+            {
+               std::cout << (char)c;
+               in.ignore();
+            }
+            else if(!std::isdigit(c) )
+            {
+               return false;
+            }
+         }
+         else
+         {
+            done = true;
+         }
+         c = in.peek();
+      }
+      return in.good();
+   }
+   bool Json::parseValue(ossim::istream& in)
+   {
+      skipWhitespace(in);
+
+      int c = in.peek();
+
+      if(c == '"')
+      {
+         return parseString(in);
+      }
+      else if(c == '{')
+      {
+         return parseObject(in);
+      }
+      else if(c == 't')
+      {
+         char buf[4];
+         in.read(buf, 4);
+         if(!in.good()||(ossimString(buf, buf+4)!="true"))
+         {
+            return false;
+         }
+         std::cout << "true";
+         return in.good();
+      }
+      else if(c == 'f')
+      {
+         char buf[5];
+         in.read(buf, 5);
+         if(!in.good()||(ossimString(buf, buf+5)!="false"))
+         {
+            return false;
+         }
+         std::cout << "false";
+         return in.good();
+      }
+      else if(c == 'n')
+      {
+         char buf[4];
+         in.read(buf, 4);
+         if(!in.good()||(ossimString(buf, buf+4)!="null"))
+         {
+            return false;
+         }
+         std::cout << "null";
+
+         return in.good();
+      }
+      else if(c=='-' || std::isdigit(c))
+      {
+         parseNumber(in);
+         return in.good();
+      }
+      else if(c == '[')
+      {
+         if(parseArray(in))
+         {
+            return in.good();
+         }
+      }
+      return false;
+   }
+   bool Json::parseArray(ossim::istream& in)
+   {
+      skipWhitespace(in);
+      int c = in.peek();
+      if(c == '[')
+      {
+         std::cout << "[";
+         in.ignore();
+         c = in.peek();
+
+         if(c == ']')
+         {
+            in.ignore();
+            std::cout << "]";
+            return true;
+         }
+         else
+         {
+            bool finished = false;
+
+            do{
+               if(parseValue(in))
+               {
+                  skipWhitespace(in);
+                  c = in.peek();
+                  in.ignore();
+                  if(c == ']')
+                  {
+                     finished = true;
+                     std::cout << "]";
+                  }
+                  else if(c != ',')
+                  {
+                     return false;
+                  }
+                  else
+                  {
+                     std::cout << ",";
+
+                  }
+               }
+
+            }while(in.good()&&!finished);
+         }
+      }
+      return in.good();
+   }
+   bool Json::parseObject(ossim::istream& in)
+   {
+      skipWhitespace(in);
+      if(!in.good())
+      {
+         return false;
+      }
+      else if(in.peek() == '{')
+      {
+         std::cout << "{";
+         in.ignore();
+         skipWhitespace(in);
+         if(in.peek() == '}')
+         {
+            std::cout << "}";
+            in.ignore();
+            return true;
+         }
+         else
+         {
+            bool keepGoing = true;
+            do{
+               if(!parseStringValue(in))
+               {
+                  return false;
+               }
+               skipWhitespace(in);
+               if(in.peek()!=',')
+               {
+                  keepGoing = false;
+               }
+               else
+               {
+                  std::cout << ",";
+                  in.ignore();
+               }
+            }while(in.good() && (keepGoing));
+
+            if(!in.good())
+            {
+               return false;
+            }
+         }
+         skipWhitespace(in);
+      }
+      else
+      {
+         skipWhitespace(in);
+      }
+      std::cout << (char)in.peek();
+      if(in.peek() == '}')
+      {
+         in.ignore();
+         return true;
+      }
+      
+      return false;
+   }
+
+}
+#endif
 int main(int argc, char *argv[])
 {
    int returnCode = 0;
@@ -104,6 +522,42 @@ int main(int argc, char *argv[])
 
    try
    {
+      ossim_uint32 maxStates=10;
+      ossim::ItemCache<ossim::ImageHandlerState> stateCache;
+      ossim_uint32 idx = 0;
+      stateCache.setMaxItemsToCache(maxStates);
+      while(idx < maxStates)
+      {
+         ossimString id = ossimString::toString(idx);
+         stateCache.addItem(id, std::make_shared<ossim::TiffHandlerState>()); 
+         ++idx;  
+      }
+      if(stateCache.getItem(ossimString::toString(0)))
+      {
+
+      }
+      if(stateCache.getItem(ossimString::toString(maxStates-1)))
+      {
+
+      }
+      stateCache.addItem(ossimString::toString(idx++), std::make_shared<ossim::TiffHandlerState>()); 
+      stateCache.addItem(ossimString::toString(idx++), std::make_shared<ossim::TiffHandlerState>()); 
+      stateCache.addItem(ossimString::toString(idx++), std::make_shared<ossim::TiffHandlerState>()); 
+
+      std::cout << "********************\n";
+      //stateCache.printLruIds(std::cout);
+      #if 0
+      ossim::Json json;
+
+      std::shared_ptr<ossim::istream> in = ossim::StreamFactoryRegistry::instance()->createIstream(argv[1]);
+      if(in)
+      {
+         if(json.parse(*in))
+         {
+            std::cout << "Parsed Jason Object\n";
+         }
+      }
+      #endif
     // add code
    }
    catch(const ossimException& e)
