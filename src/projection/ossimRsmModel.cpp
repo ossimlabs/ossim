821e9c09ea0ad8b7464cb3de2b01b472700bdd54
Garrett Potts <gpotts@radiantblue.com>
2018-08-14 10:30:26 -0400

    Removed the wrap test in put in ossimGpt

--- src/projection/ossimRsmModel.cpp
+++ src/projection/ossimRsmModel.cpp
@@ -338,10 +338,7 @@ void ossimRsmModel::lineSampleHeightToWorld(const ossimDpt& image_point,
    gpt.lon = ossim::radiansToDegrees(nlon*m_pca[pcaIndex].m_xnrmsf + m_pca[pcaIndex].m_xnrmo);
    gpt.hgt = (nhgt * m_pca[pcaIndex].m_znrmsf) + m_pca[pcaIndex].m_znrmo; //ellHeight;
 
-   if(!(ossim::isnan(gpt.lat) || ossim::isnan(gpt.lon)))
-   {
-      gpt.wrap();
-   }
+   gpt.wrap();
    //---
    // Note: See above note. Added in wrap call. Longitude was coming out 242
    // when should have been -118. (drb - 22 May 2015)
