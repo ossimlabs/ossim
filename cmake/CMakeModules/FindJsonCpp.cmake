b468749f203cc808f1b4e8ce9311507365cd7749
lbartoletti <lbartoletti@users.noreply.github.com>
2019-05-10 10:08:02 +0200

    Update FindJsonCpp.cmake
    Fixes #214 
    
    It should be worked for any system

--- cmake/CMakeModules/FindJsonCpp.cmake
+++ cmake/CMakeModules/FindJsonCpp.cmake
@@ -16,7 +16,7 @@ set(JSONCPP_FOUND "NO")
 set(OSSIM_HAS_JSONCPP 0)
 
 find_path( JSONCPP_INCLUDE_DIR json/json.h
-          PATHS "${CMAKE_INSTALL_PREFIX}" "/usr/include/jsoncpp" )
+          PATHS "${CMAKE_INSTALL_PREFIX}/include/jsoncpp" )
 
 find_library( JSONCPP_LIBRARY NAMES "jsoncpp")
 
