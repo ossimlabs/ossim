66a74dff25e3d64357f6c69fa70bdcfed96fc41a
Garrett Potts <gpotts@radiantblue.com>
2020-03-30 13:29:08 -0400

    Added sample usage for the Xml and Json formatter

--- include/ossim/base/KwlNodeJsonFormatter.h
+++ include/ossim/base/KwlNodeJsonFormatter.h
@@ -4,41 +4,78 @@
 
 namespace ossim
 {
-   class KwlNodeJsonFormatter : public KwlNodeFormatter
-   {
-   public:
-      KwlNodeJsonFormatter(std::shared_ptr<KwlNode> kwlNode,
-                           const ossimString &rootTag = ossimString(""))
-          : KwlNodeFormatter(kwlNode),
-            m_rootTag(rootTag)
-      {
-      }
-      KwlNodeJsonFormatter(const ossimKeywordlist &kwl,
-                           const ossimString &rootTag = ossimString(""))
-          : KwlNodeFormatter(kwl),
-            m_rootTag(rootTag) {}
-      virtual void write(std::ostream &out,
-                        const FormatHints &hints = FormatHints()) const;
+/*
+* @code
+ #include <ossim/base/ossimKeywordlist.h>
+ #include <ossim/base/KwlNode.h>
+ #include <ossim/base/KwlNodeXmlFormatter.h>
+ int main()int argc, char* argv[])
+ {
+   ossimKeywordlist kwl;
 
-   protected:
-      void toJSON(std::ostream &out, const KwlNode *currentNode,
-                  int indent = 0,
-                  const FormatHints &hints = FormatHints()) const;
-      void toJSONAttributes(std::ostream &out,
-                           const KwlNode *currentNode,
-                           int indent = 0,
-                           const FormatHints &hints = FormatHints()) const;
-      void toJSONChildren(std::ostream &out,
+   kwl.add("tiff.@version", "1");
+   kwl.add("tiff.what2", "hey1");
+   kwl.add("tiff.what1.@f", "hey2");
+   kwl.add("tiff.what1", "hey2");
+   kwl.add("tiff.what1.ff", "hey3");
+   kwl.add("tiff.what10014.ads1.dd", "hey3");
+   kwl.add("tiff.what10015.ads2.dd", "hey4");
+   kwl.add("tiff.what2", "hey2");
+   kwl.add("tiff.what3", "hey3");
+
+   std::shared_ptr<ossim::KwlNodeFormatter> formatter =
+       std::make_shared<ossim::KwlNodeJsonFormatter>(kwl);
+
+   formatter->write(std::cout,
+                    ossim::KwlNodeFormatter::FormatHints(3, true, true, true));
+
+   std::ostringstream out;
+   formatter->write(out,
+                    ossim::KwlNodeFormatter::FormatHints(3, true, true, true));
+
+}
+* @endcode
+* SAMPLE OUTPUT:
+*
+* @code
+{"tiff" : {"what" : [{"ff" : "hey3"},"hey2","hey3",{"ads" : [{"dd" : "hey3"}]},{"ads" : [{"dd" : "hey4"}]}],"version" : "1"}}
+* @endcode
+*/
+class KwlNodeJsonFormatter : public KwlNodeFormatter
+{
+public:
+   KwlNodeJsonFormatter(std::shared_ptr<KwlNode> kwlNode,
+                        const ossimString &rootTag = ossimString(""))
+       : KwlNodeFormatter(kwlNode),
+         m_rootTag(rootTag)
+   {
+   }
+   KwlNodeJsonFormatter(const ossimKeywordlist &kwl,
+                        const ossimString &rootTag = ossimString(""))
+       : KwlNodeFormatter(kwl),
+         m_rootTag(rootTag) {}
+   virtual void write(std::ostream &out,
+                      const FormatHints &hints = FormatHints()) const;
+
+protected:
+   void toJSON(std::ostream &out, const KwlNode *currentNode,
+               int indent = 0,
+               const FormatHints &hints = FormatHints()) const;
+   void toJSONAttributes(std::ostream &out,
+                         const KwlNode *currentNode,
+                         int indent = 0,
+                         const FormatHints &hints = FormatHints()) const;
+   void toJSONChildren(std::ostream &out,
+                       const KwlNode *currentNode,
+                       int indent = 0,
+                       const FormatHints &hints = FormatHints()) const;
+   void toJSONArrayType(std::ostream &out,
                         const KwlNode *currentNode,
                         int indent = 0,
                         const FormatHints &hints = FormatHints()) const;
-      void toJSONArrayType(std::ostream &out,
-                           const KwlNode *currentNode,
-                           int indent = 0,
-                           const FormatHints &hints = FormatHints()) const;
-      ossimString fixValue(const ossimString &value) const;
+   ossimString fixValue(const ossimString &value) const;
 
-      ossimString m_rootTag;
+   ossimString m_rootTag;
    };
 }
 #endif
--- include/ossim/base/KwlNodeXmlFormatter.h
+++ include/ossim/base/KwlNodeXmlFormatter.h
@@ -1,6 +1,73 @@
 #ifndef ossimKwlNodeXmlFormatter_HEADER
 #define ossimKwlNodeXmlFormatter_HEADER
+
 #include <ossim/base/KwlNodeFormatter.h>
+
+/*
+* @code
+ #include <ossim/base/ossimKeywordlist.h>
+ #include <ossim/base/KwlNode.h>
+ #include <ossim/base/KwlNodeXmlFormatter.h>
+ int main()int argc, char* argv[])
+ {
+   ossimKeywordlist kwl;
+
+   kwl.add("tiff.@version", "1");
+   kwl.add("tiff.what2", "hey1");
+   kwl.add("tiff.what1.@f", "hey2");
+   kwl.add("tiff.what1", "hey2");
+   kwl.add("tiff.what1.ff", "hey3");
+   kwl.add("tiff.what10014.ads1.dd", "hey3");
+   kwl.add("tiff.what10015.ads2.dd", "hey4");
+   kwl.add("tiff.what2", "hey2");
+   kwl.add("tiff.what3", "hey3");
+
+   std::shared_ptr<ossim::KwlNodeFormatter> formatter =
+       std::make_shared<ossim::KwlNodeXmlFormatter>(kwl);
+
+   formatter->write(std::cout,
+                    ossim::KwlNodeFormatter::FormatHints(3, true, true, true));
+
+   std::ostringstream out;
+   formatter->write(out,
+                    ossim::KwlNodeFormatter::FormatHints(3, true, true, true));
+
+}
+* @endcode
+* SAMPLE OUTPUT:
+*
+* @code
+<?xml version='1.0'?>
+<TIFF version="1">
+   <WHAT f="hey2">
+      <ff>
+         hey3
+      </ff>
+      hey2
+   </WHAT>
+   <what>
+      hey2
+   </what>
+   <what>
+      hey3
+   </what>
+   <WHAT>
+      <ADS>
+         <dd>
+            hey3
+         </dd>
+      </ADS>
+   </WHAT>
+   <WHAT>
+      <ADS>
+         <dd>
+            hey4
+         </dd>
+      </ADS>
+   </WHAT>
+</TIFF>
+* @endcode
+*/
 
 namespace ossim
 {
