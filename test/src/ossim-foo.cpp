25a22e46e976f73482c92d98995b78948c97f295
David Burken <dburken@gmail.com>
2023-08-29 16:12:38 -0400

    Bumped compiler to c++17.

--- test/src/ossim-foo.cpp
+++ test/src/ossim-foo.cpp
@@ -14,30 +14,102 @@
 // $Id$
 //---
 
+// ossim includes:  These are here just to save time/typing...
+#include <ossim/base/ossimApplicationUsage.h>
+#include <ossim/base/ossimArgumentParser.h>
+#include <ossim/base/ossimConnectableObject.h>
+#include <ossim/base/ossimCsvFile.h>
+#include <ossim/base/ossimDate.h>
+#include <ossim/base/ossimDpt.h>
+#include <ossim/base/ossimDrect.h>
+#include <ossim/base/ossimException.h>
+#include <ossim/base/ossimFilename.h>
+#include <ossim/base/ossimGrect.h>
+#include <ossim/base/ossimIpt.h>
+#include <ossim/base/ossimIrect.h>
+#include <ossim/base/ossimKeywordlist.h>
+#include <ossim/base/ossimKeywordNames.h>
+#include <ossim/base/ossimNotify.h>
+#include <ossim/base/ossimObjectFactory.h>
+#include <ossim/base/ossimObjectFactoryRegistry.h>
+#include <ossim/base/ossimProperty.h>
+#include <ossim/base/ossimRefPtr.h>
+#include <ossim/base/ossimString.h>
+#include <ossim/base/ossimScalarTypeLut.h>
+#include <ossim/base/ossimStdOutProgress.h>
+#include <ossim/base/ossimStreamFactoryRegistry.h>
+#include <ossim/base/ossimStringProperty.h>
+#include <ossim/base/ossimTimer.h>
+#include <ossim/base/ossimTrace.h>
+#include <ossim/base/ossimUrl.h>
+#include <ossim/base/ossimVisitor.h>
+#include <ossim/base/ossimEcefPoint.h>
+#include <ossim/base/ossimEcefVector.h>
+#include <ossim/base/ossim2dBilinearTransform.h>
+
+#include <ossim/imaging/ossimNitfTileSource.h>
+#include <ossim/imaging/ossimBrightnessContrastSource.h>
+#include <ossim/imaging/ossimBumpShadeTileSource.h>
+#include <ossim/imaging/ossimFilterResampler.h>
+#include <ossim/imaging/ossimFusionCombiner.h>
+#include <ossim/imaging/ossimImageData.h>
+#include <ossim/imaging/ossimImageFileWriter.h>
+#include <ossim/imaging/ossimImageGeometry.h>
+#include <ossim/imaging/ossimImageHandler.h>
+#include <ossim/imaging/ossimImageMosaic.h>
+#include <ossim/imaging/ossimImageRenderer.h>
+#include <ossim/imaging/ossimImageSource.h>
+#include <ossim/imaging/ossimImageSourceFilter.h>
+#include <ossim/imaging/ossimImageToPlaneNormalFilter.h>
+#include <ossim/imaging/ossimImageWriterFactoryRegistry.h>
+#include <ossim/imaging/ossimIndexToRgbLutFilter.h>
+#include <ossim/imaging/ossimRectangleCutFilter.h>
+#include <ossim/imaging/ossimScalarRemapper.h>
+#include <ossim/imaging/ossimSFIMFusion.h>
+#include <ossim/imaging/ossimSingleImageChain.h>
+#include <ossim/imaging/ossimTwoColorView.h>
+#include <ossim/imaging/ossimImageSourceFactoryRegistry.h>
+#include <ossim/imaging/ossimImageHandlerRegistry.h>
+
+#include <ossim/init/ossimInit.h>
+
+#include <ossim/projection/ossimEquDistCylProjection.h>
+#include <ossim/projection/ossimImageViewAffineTransform.h>
+#include <ossim/projection/ossimMapProjection.h>
+#include <ossim/projection/ossimProjection.h>
+#include <ossim/projection/ossimProjectionFactoryRegistry.h>
+#include <ossim/projection/ossimUtmProjection.h>
+
+#include <ossim/support_data/ossimSrcRecord.h>
+#include <ossim/support_data/ossimWkt.h>
+
+#include <ossim/base/Barrier.h>
+#include <ossim/base/Block.h>
+#include <ossim/base/Thread.h>
+
+// Put your includes here:
+
+// System includes:
+#include <cmath>
+#include <cstdint>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <string_view>
+#include <typeinfo>
+
 // System includes:
 #include <cmath>
 #include <memory>
 #include <string>
+#include <string_view>
 #include <sstream>
 #include <iostream>
 #include <iomanip>
 #include <cstring>
-
-// Put your includes here:
-#include <ossim/init/ossimInit.h>
-#include <ossim/base/ossimConstants.h>
-#include <ossim/base/ossimString.h>
-#include <ossim/base/ossimCommon.h>
-#include <ossim/imaging/ossimImageHandlerRegistry.h>
-#include <ossim/imaging/ossimImageGeometryRegistry.h>
-#include <ossim/imaging/ossimImageGeometry.h>
-#include <ossim/imaging/ossimImageHandler.h>
-#include <ossim/base/ossimArgumentParser.h>
-#include <ossim/base/ossimApplicationUsage.h>
-#include <ossim/imaging/ossimImageWriterFactoryRegistry.h>
-#include <ossim/base/ossimTimer.h>
-
-using namespace std;
 
 int main(int argc, char *argv[])
 {
@@ -50,7 +122,7 @@ int main(int argc, char *argv[])
    ossimInit::instance()->initialize(ap);
    try
    {
-       /* code */
+      // Put your code here:
    }
    catch(const std::exception& e)
    {
@@ -61,9 +133,9 @@ int main(int argc, char *argv[])
 
    ossimNotify(ossimNotifyLevel_NOTICE)
        << "elapsed time in seconds: "
-       << std::setiosflags(ios::fixed)
+       << std::setiosflags(std::ios::fixed)
        << std::setprecision(3)
-       << timer.delta_s(tickStart, tickEnd) << endl;
+       << timer.delta_s(tickStart, tickEnd) << std::endl;
 
    return 0;
 }
