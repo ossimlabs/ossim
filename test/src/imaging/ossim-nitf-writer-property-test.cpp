ossimlabs/ossim

commit a694e18f39268bd6a80ca1da57d4665cdb7e833e

Updated ossimNitfWriterBase.

src/imaging/ossimNitfWriterBase.cpp
@@ -29,9 +29,11 @@
 #include <ossim/support_data/ossimNitfBlockaTag.h>
 #include <ossim/support_data/ossimNitfFileHeader.h>
 #include <ossim/support_data/ossimNitfFileHeaderV2_X.h>
+// #include <ossim/support_data/ossimNitfFileHeaderV2_1.h>
 #include <ossim/support_data/ossimNitfGeolobTag.h>
 #include <ossim/support_data/ossimNitfImageHeader.h>
 #include <ossim/support_data/ossimNitfImageHeaderV2_X.h>
+// #include <ossim/support_data/ossimNitfImageHeaderV2_1.h>
 #include <ossim/support_data/ossimNitfRegisteredTag.h>
 #include <ossim/support_data/ossimNitfTagInformation.h>
 #include <ossim/base/ossim2dBilinearTransform.h>
@@ -43,23 +45,29 @@ static const char ENABLE_RPCB_KW[]   = "enable_rpcb_tag";
 static const char ENABLE_GEOLOB_KW[] = "enable_geolob_tag";
 
 RTTI_DEF1(ossimNitfWriterBase, "ossimNitfWriterBase", ossimImageFileWriter)
-
+   
 static ossimTrace traceDebug(ossimString("ossimNitfWriterBase:debug"));
 
 ossimNitfWriterBase::ossimNitfWriterBase()
    : ossimImageFileWriter(),
-     theEnableRpcbTagFlag(false),
-     theEnableBlockaTagFlag(true),
-     theEnableGeolobTagFlag(true)
+     m_enableRpcbTagFlag(false),
+     m_enableBlockaTagFlag(true),
+     m_enableGeolobTagFlag(true),
+     m_tags(0),
+     m_fileHeaderProps(0),
+     m_imageHeaderProps(0)
 {
 }
 
 ossimNitfWriterBase::ossimNitfWriterBase(const ossimFilename& filename,
                                          ossimImageSource* inputSource)
    : ossimImageFileWriter(filename, inputSource, 0),
-     theEnableRpcbTagFlag(false),
-     theEnableBlockaTagFlag(true),
-     theEnableGeolobTagFlag(true)
+     m_enableRpcbTagFlag(false),
+     m_enableBlockaTagFlag(true),
+     m_enableGeolobTagFlag(true),
+     m_tags(0),
+     m_fileHeaderProps(0),
+     m_imageHeaderProps(0)
 {
 }
 
@@ -75,15 +83,15 @@ void ossimNitfWriterBase::setProperty(ossimRefPtr<ossimProperty> property)
 
       if (name == ENABLE_RPCB_KW)
       {
-         theEnableRpcbTagFlag = property->valueToString().toBool();
+         m_enableRpcbTagFlag = property->valueToString().toBool();
       }
       else if (name == ENABLE_BLOCKA_KW)
       {
-         theEnableBlockaTagFlag = property->valueToString().toBool();
+         m_enableBlockaTagFlag = property->valueToString().toBool();
       }
       else if (name == ENABLE_GEOLOB_KW)
       {
-         theEnableGeolobTagFlag = property->valueToString().toBool();
+         m_enableGeolobTagFlag = property->valueToString().toBool();
       }
       else
       {
@@ -96,18 +104,18 @@ ossimRefPtr<ossimProperty> ossimNitfWriterBase::getProperty(
    const ossimString& name)const
 {
    ossimRefPtr<ossimProperty> result = 0;
-
+   
    if(name == ENABLE_BLOCKA_KW)
    {
-      result = new ossimBooleanProperty(name, theEnableBlockaTagFlag);
+      result = new ossimBooleanProperty(name, m_enableBlockaTagFlag);
    }
    else if (name == ENABLE_GEOLOB_KW)
    {
-      result = new ossimBooleanProperty(name, theEnableGeolobTagFlag);
+      result = new ossimBooleanProperty(name, m_enableGeolobTagFlag);
    }
    else if(name == ENABLE_RPCB_KW)
    {
-      result = new ossimBooleanProperty(name, theEnableRpcbTagFlag);
+      result = new ossimBooleanProperty(name, m_enableRpcbTagFlag);
    }
    else
    {
@@ -131,9 +139,9 @@ void ossimNitfWriterBase::getPropertyNames(
 bool ossimNitfWriterBase::saveState(ossimKeywordlist& kwl,
                                     const char* prefix) const
 {
-   kwl.add(prefix, ENABLE_BLOCKA_KW, theEnableBlockaTagFlag, true);
-   kwl.add(prefix, ENABLE_GEOLOB_KW, theEnableGeolobTagFlag, true);
-   kwl.add(prefix, ENABLE_RPCB_KW, theEnableRpcbTagFlag, true);
+   kwl.add(prefix, ENABLE_BLOCKA_KW, m_enableBlockaTagFlag, true);
+   kwl.add(prefix, ENABLE_GEOLOB_KW, m_enableGeolobTagFlag, true);
+   kwl.add(prefix, ENABLE_RPCB_KW, m_enableRpcbTagFlag, true);   
 
    return ossimImageFileWriter::saveState(kwl, prefix);
 }
@@ -149,7 +157,7 @@ bool ossimNitfWriterBase::loadState(const ossimKeywordlist& kwl,
    if(lookup)
    {
       ossimString os = lookup;
-      theEnableBlockaTagFlag = os.toBool();
+      m_enableBlockaTagFlag = os.toBool();
    }
 
    // Look for the geolob enable flag keyword.
@@ -157,14 +165,14 @@ bool ossimNitfWriterBase::loadState(const ossimKeywordlist& kwl,
    if(lookup)
    {
       ossimString os = lookup;
-      theEnableGeolobTagFlag = os.toBool();
+      m_enableGeolobTagFlag = os.toBool();
    }
 
    lookup = kwl.find(prefix, ENABLE_RPCB_KW);
    if(lookup)
    {
       ossimString os = lookup;
-      theEnableRpcbTagFlag = os.toBool();
+      m_enableRpcbTagFlag = os.toBool();
    }
 
    return ossimImageFileWriter::loadState(kwl, prefix);
@@ -173,80 +181,100 @@ bool ossimNitfWriterBase::loadState(const ossimKeywordlist& kwl,
 void ossimNitfWriterBase::writeGeometry(ossimNitfImageHeaderV2_X* hdr,
                                         ossimImageSourceSequencer* seq)
 {
-   if (!hdr || !seq)
-      return;
-
-   ossimRefPtr<ossimImageGeometry> geom = theInputConnection->getImageGeometry();
-   ossimKeywordlist kwl;
-   if (!geom || !geom->hasProjection())
-      return;
-
-   // Get the requested bounding rectangles.
-   ossimIrect rect = seq->getBoundingRect();
-
-   // See if it's a map projection; else, a sensor model.
-   ossimMapProjection* mapProj = PTR_CAST(ossimMapProjection, geom->getProjection());
-   if (mapProj)
+   if (hdr && seq)
    {
-      // Use map info to get the corners.
-      ossimMapProjectionInfo mapInfo(mapProj, rect);
-      mapInfo.setPixelType(OSSIM_PIXEL_IS_AREA);
+      ossimRefPtr<ossimImageGeometry> geom = theInputConnection->getImageGeometry();
 
-      // See if it's utm.
-      ossimUtmProjection* utmProj = PTR_CAST(ossimUtmProjection,
-                                             mapProj);
-      if(utmProj)
+      if (geom.valid()&&geom->hasProjection())
       {
-         ossimDpt ul = mapInfo.ulEastingNorthingPt();
-         ossimDpt ur = mapInfo.urEastingNorthingPt();
-         ossimDpt lr = mapInfo.lrEastingNorthingPt();
-         ossimDpt ll = mapInfo.llEastingNorthingPt();
-
-         if(utmProj->getHemisphere() == 'N')
+         // Get the requested bounding rectangles.
+         ossimIrect rect = seq->getBoundingRect();
+         
+         // See if it's a map projection; else, a sensor model.
+         ossimMapProjection* mapProj = PTR_CAST(ossimMapProjection, geom->getProjection());
+         if (mapProj)
          {
-            hdr->setUtmNorth(utmProj->getZone(), ul, ur, lr, ll);
+            // Use map info to get the corners.
+            ossimMapProjectionInfo mapInfo(mapProj, rect);
+            mapInfo.setPixelType(OSSIM_PIXEL_IS_AREA);
+            
+            // See if it's utm.
+            ossimUtmProjection* utmProj = PTR_CAST(ossimUtmProjection, mapProj);
+            if(utmProj)
+            {
+               ossimDpt ul = mapInfo.ulEastingNorthingPt();
+               ossimDpt ur = mapInfo.urEastingNorthingPt();
+               ossimDpt lr = mapInfo.lrEastingNorthingPt();
+               ossimDpt ll = mapInfo.llEastingNorthingPt();
+               
+               if(utmProj->getHemisphere() == 'N')
+               {
+                  hdr->setUtmNorth(utmProj->getZone(), ul, ur, lr, ll);
+               }
+               else
+               {
+                  hdr->setUtmSouth(utmProj->getZone(), ul, ur, lr, ll);
+               }
+            }
+            else
+            {
+               ossimGpt ul = mapInfo.ulGroundPt();
+               ossimGpt ur = mapInfo.urGroundPt();
+               ossimGpt lr = mapInfo.lrGroundPt();
+               ossimGpt ll = mapInfo.llGroundPt();
+               hdr->setGeographicLocationDms(ul, ur, lr, ll);
+            }
+
+            if (m_enableBlockaTagFlag)
+            {
+               addBlockaTag(mapInfo, hdr);
+            }
+
+            if ( m_enableGeolobTagFlag )
+            {
+               addGeolobTag( mapInfo, hdr );
+            }
          }
          else
          {
-            hdr->setUtmSouth(utmProj->getZone(), ul, ur, lr, ll);
+            ossimGpt ul, ur, lr, ll;
+            bool status = geom->getCornerGpts(ul, ur, lr, ll);
+            if (status)
+            {
+                hdr->setGeographicLocationDms(ul, ur, lr, ll);
+            }
+         }
+         
+         if (m_enableRpcbTagFlag)
+         {
+            addRpcbTag(rect, geom->getProjection(), hdr);
+         }
+         
+      } // matches:  if (proj.valid())
+
+      if ( m_tags.size() )
+      {
+         const auto& cv = m_tags;
+         for ( auto&& i : cv )
+         {
+            // Add the tag to the header.
+            ossimNitfTagInformation tagInfo(i);
+            hdr->addTag(i);
          }
       }
-      else
+
+#if 0 /* Moved to addImageHeaderProperties */
+      if ( m_imageHeaderProps.size() )
       {
-         ossimGpt ul = mapInfo.ulGroundPt();
-         ossimGpt ur = mapInfo.urGroundPt();
-         ossimGpt lr = mapInfo.lrGroundPt();
-         ossimGpt ll = mapInfo.llGroundPt();
-         hdr->setGeographicLocationDms(ul, ur, lr, ll);
+         const auto& cv = m_imageHeaderProps;
+         for ( auto&& i : cv )
+         {
+            hdr->setProperty( i );
+         }
       }
-
-      if (theEnableBlockaTagFlag)
-      {
-         addBlockaTag(mapInfo, hdr);
-      }
-
-      if ( theEnableGeolobTagFlag )
-      {
-         addGeolobTag( mapInfo, hdr );
-      }
-   }
-   else
-   {
-      // Not a map projection, probably sensor model:
-      ossimGpt ul, ur, lr, ll;
-      bool status = geom->getCornerGpts(ul, ur, lr, ll);
-      if (status)
-      {
-         hdr->setGeographicLocationDms(ul, ur, lr, ll);
-      }
-   }
-
-   if (theEnableRpcbTagFlag)
-      addRpcbTag(rect, geom->getProjection(), hdr);
-
-   // Now deal with possible 2D transform in the image geometry. Use it to populate ICHIPB block:
-   addIchipbTag(geom.get(), hdr);
-
+#endif
+      
+   } // matches: if (hdr && seq)
 }
 
 void ossimNitfWriterBase::addBlockaTag(ossimMapProjectionInfo& mapInfo,
@@ -256,31 +284,31 @@ void ossimNitfWriterBase::addBlockaTag(ossimMapProjectionInfo& mapInfo,
    {
       // Capture the current pixel type.
       ossimPixelType originalPixelType = mapInfo.getPixelType();
-
+      
       // This tag wants corners as area:
       mapInfo.setPixelType(OSSIM_PIXEL_IS_AREA);
-
+      
       // Stuff the blocka tag which has six digit precision.
       ossimRefPtr<ossimNitfBlockaTag> blockaTag = new ossimNitfBlockaTag();
-
+      
       // Set the block number.
       blockaTag->setBlockInstance(1);
-
+      
       // Set the number of lines.
       blockaTag->setLLines(mapInfo.linesPerImage());
-
+      
       // Set first row, first column.
       blockaTag->setFrfcLoc(ossimDpt(mapInfo.ulGroundPt()));
-
+      
       // Set first row, last column.
       blockaTag->setFrlcLoc(ossimDpt(mapInfo.urGroundPt()));
-
+      
       // Set last row, last column.
       blockaTag->setLrlcLoc(ossimDpt(mapInfo.lrGroundPt()));
-
+      
       // Set last row, first column.
       blockaTag->setLrfcLoc(ossimDpt(mapInfo.llGroundPt()));
-
+      
       if (traceDebug())
       {
          ossimNotify(ossimNotifyLevel_DEBUG)
@@ -288,15 +316,15 @@ void ossimNitfWriterBase::addBlockaTag(ossimMapProjectionInfo& mapInfo,
             << "\nBLOCKA Tag:" << *((ossimObject*)(blockaTag.get()))
             << std::endl;
       }
-
+      
       // Add the tag to the header.
       ossimRefPtr<ossimNitfRegisteredTag> blockaTagRp = blockaTag.get();
       ossimNitfTagInformation blockaTagInfo(blockaTagRp);
       hdr->addTag(blockaTagInfo);
-
+      
       // Reset the pixel type to original value
       mapInfo.setPixelType(originalPixelType);
-
+      
    } // matches: if (hdr)
 }
 
@@ -373,37 +401,37 @@ void ossimNitfWriterBase::addRpcbTag(const ossimIrect& rect,
    if (proj && hdr)
    {
       bool useElevation = false;
-
+      
       if (PTR_CAST(ossimMapProjection, proj))
       {
          // If we're already map projected turn the elevation off.
          useElevation = false;
       }
-
+      
       // Make an rpc solver.
       ossimRefPtr<ossimRpcSolver> rs = new ossimRpcSolver(useElevation);
-
+      
       // Compute the coefficients.
       rs->solveCoefficients(ossimDrect(rect), proj, 64, 64);
-
+      
       // Add the tag.
       ossimRefPtr<ossimNitfRegisteredTag> tag = rs->getNitfRpcBTag();
       ossimNitfTagInformation tagInfo(tag);
       hdr->addTag(tagInfo);
-
+      
       if (traceDebug())
       {
          ossimNotify(ossimNotifyLevel_DEBUG)
             << "ossimNitfWriterBase::addRpcbTag DEBUG:"
             << "\nRPCB Tag:" << *((ossimObject*)(tag.get()))
             << "\nProjection:\n";
-
+         
          proj->print(ossimNotify(ossimNotifyLevel_DEBUG));
-
+         
          ossimNotify(ossimNotifyLevel_DEBUG)
             << "\nRect: " << rect << std::endl;
       }
-
+      
    } // matches: if (proj && hdr)
 }
 
@@ -421,9 +449,9 @@ void ossimNitfWriterBase::setComplexityLevel(std::streamoff endPosition,
       const std::streamoff MB50 = 50   * MB;
       const std::streamoff GIG  = 1000 * MB;
       const std::streamoff GIG2 = 2    * GIG;
-
+      
       ossimString complexity = "03"; // Less than 50 mb.
-
+      
       if ( (endPosition >= MB50) && (endPosition < GIG) )
       {
          complexity = "05";
@@ -436,7 +464,7 @@ void ossimNitfWriterBase::setComplexityLevel(std::streamoff endPosition,
       {
          complexity = "07";
       }
-
+      
       hdr->setComplexityLevel(complexity);
    }
 }
@@ -461,8 +489,9 @@ ossimString ossimNitfWriterBase::getExtension() const
    return ossimString("ntf");
 }
 
-void ossimNitfWriterBase::addRegisteredTag(ossimRefPtr<ossimNitfRegisteredTag> /* registeredTag */)
+void ossimNitfWriterBase::addRegisteredTag(ossimRefPtr<ossimNitfRegisteredTag> registeredTag )
 {
+   m_tags.push_back( registeredTag );
 }
 
 void ossimNitfWriterBase::addRegisteredTag(ossimRefPtr<ossimNitfRegisteredTag> /* registeredTag */, bool /* unique */)
@@ -473,6 +502,7 @@ void ossimNitfWriterBase::addRegisteredTag(ossimRefPtr<ossimNitfRegisteredTag> /
 {
 }
 
+#if 0 /* Not called by anyone. */
 void ossimNitfWriterBase::setFileHeaderV2_1(ossimRefPtr<ossimNitfFileHeaderV2_1>, bool /* preferSource */)
 {
 }
@@ -480,6 +510,7 @@ void ossimNitfWriterBase::setFileHeaderV2_1(ossimRefPtr<ossimNitfFileHeaderV2_1>
 void ossimNitfWriterBase::setImageHeaderV2_1(ossimRefPtr<ossimNitfImageHeaderV2_1>, bool /* preferSource */)
 {
 }
+#endif
 
 void ossimNitfWriterBase::initializeDefaultsFromConfigFile( ossimNitfFileHeaderV2_X* fileHdr,
                                                             ossimNitfImageHeaderV2_X* imgHdr )
@@ -494,6 +525,44 @@ void ossimNitfWriterBase::initializeDefaultsFromConfigFile( ossimNitfFileHeaderV
       {
          fileHdr->loadState( kwl, "nitf.file." );
          imgHdr->loadState( kwl, "nitf.image." );
+      }
+   }
+}
+
+// public:
+void ossimNitfWriterBase::setFileHeaderProperty( ossimRefPtr<ossimProperty> property )
+{
+   m_fileHeaderProps.push_back( property );
+}
+
+// public:
+void ossimNitfWriterBase::setImageHeaderProperty( ossimRefPtr<ossimProperty> property )
+{
+   m_imageHeaderProps.push_back( property );
+}
+
+// protected:
+void ossimNitfWriterBase::addFileHeaderProperties( ossimNitfFileHeaderV2_X* hdr )
+{
+   if ( hdr && m_fileHeaderProps.size() )
+   {
+      const auto& cv = m_fileHeaderProps;
+      for ( auto&& i : cv )
+      {
+         hdr->setProperty( i );
+      }
+   }
+}
+
+// protected:
+void ossimNitfWriterBase::addImageHeaderProperties( ossimNitfImageHeaderV2_X* hdr )
+{
+   if ( hdr && m_imageHeaderProps.size() )
+   {
+      const auto& cv = m_imageHeaderProps;
+      for ( auto&& i : cv )
+      {
+         hdr->setProperty( i );
       }
    }
 }

include/ossim/imaging/ossimNitfWriterBase.h
@@ -15,10 +15,7 @@
 #include <ossim/imaging/ossimImageFileWriter.h>
 #include <ossim/base/ossimRefPtr.h>
 #include <ossim/support_data/ossimNitfRegisteredTag.h>
-#include <ossim/support_data/ossimNitfFileHeaderV2_1.h>
-#include <ossim/support_data/ossimNitfImageHeaderV2_1.h>
 #include <vector>
-#include <ossim/base/ossim2dBilinearTransform.h>
 
 class ossimFilename;
 class ossimImageSourceSequencer;
@@ -122,8 +119,22 @@ public:
    virtual void addRegisteredTag(ossimRefPtr<ossimNitfRegisteredTag> registeredTag, bool unique);
    virtual void addRegisteredTag(ossimRefPtr<ossimNitfRegisteredTag> registeredTag, bool unique, const ossim_uint32& ownerIndex, const ossimString& tagType);
 
+#if 0 /* Not called by anyone. */
    virtual void setFileHeaderV2_1(ossimRefPtr<ossimNitfFileHeaderV2_1>, bool preferSource=false);
    virtual void setImageHeaderV2_1(ossimRefPtr<ossimNitfImageHeaderV2_1>, bool preferSource=false);
+#endif
+
+   /**
+    * @brief Set file header property.
+    * @param property The property to set.
+    */
+   virtual void setFileHeaderProperty( ossimRefPtr<ossimProperty> property );
+
+   /**
+    * @brief Set image header property.
+    * @param property The property to set.
+    */
+   virtual void setImageHeaderProperty( ossimRefPtr<ossimProperty> property );
 
 protected:
 
@@ -196,23 +207,40 @@ protected:
     * found in preferences.
     */
    virtual void initializeDefaultsFromConfigFile(
-      ossimNitfFileHeaderV2_X* fileHdr,
-      ossimNitfImageHeaderV2_X* imgHdr );   
+      ossimNitfFileHeaderV2_X* fileHdr, ossimNitfImageHeaderV2_X* imgHdr );
+
+   /**
+    * @brief Sets stored properties in nitf file header.
+    * @see setFileHeaderProperty(...)
+    * @param hdr
+    */
+   void addFileHeaderProperties( ossimNitfFileHeaderV2_X* hdr );
+
+   /**
+    * @brief Sets stored properties in nitf image header.
+    * @see setImageHeaderProperty(...)
+    * @param hdr
+    */   
+   void addImageHeaderProperties( ossimNitfImageHeaderV2_X* hdr );
 
    /** @brief If true user wants to set RPC00B tag. (DEFAULT = false) */
-   bool theEnableRpcbTagFlag;
+   bool m_enableRpcbTagFlag;
    
    /**
     * @brief If true user wants to set BLOCKA tag. (DEFAULT = true)
     * Currently only valid for map projected images.
     */
-   bool theEnableBlockaTagFlag;
+   bool m_enableBlockaTagFlag;
 
    /**
     * @brief If true user wants to set GEOLOG tag. (DEFAULT = true)
     * This will only be set if a geographic projection.
     */
-   bool theEnableGeolobTagFlag;
+   bool m_enableGeolobTagFlag;
+
+   std::vector< ossimRefPtr<ossimNitfRegisteredTag> > m_tags;
+   std::vector< ossimRefPtr<ossimProperty> > m_fileHeaderProps;
+   std::vector< ossimRefPtr<ossimProperty> > m_imageHeaderProps;
 
 private:
 

commit 49d3d42553563195d0f3e1de1f4cc1adf7447343

fix: setFileHeaderProperty/setImageHeaderProperty never reached the output
Both setters push onto m_fileHeaderProps / m_imageHeaderProps, whose only
consumers are addFileHeaderProperties() / addImageHeaderProperties(). Nothing
called either method, so the public API silently discarded everything set
through it -- ask for FSCLAS "U" and the file still gets a space.

The image-header half regressed in a694e18f "Updated ossimNitfWriterBase"
(2025-06-13). It moved the working code out of writeGeometry(), leaving

    #if 0 /* Moved to addImageHeaderProperties */

behind, and touched only ossimNitfWriterBase.{h,cpp} -- so no writer was
updated to call the new method. The file-header half never had a call site at
all.

Both writers now apply them in writeFile(), beside the existing
writeGeometry() call and before any header is serialised. The dead #if 0 block
is removed rather than re-enabled: it covered only the image half, only for
writers that call writeGeometry(), and put property application inside a
function named for geometry. Moving it out was right; only the wiring was
missing.

ossim-plugins needs the same one-line pair in ossimKakaduNitfWriter, which
serialises its file header before it calls writeGeometry(); PR to follow.

Adds ossim-nitf-writer-property-test, which writes a small NITF with FSCLAS
set through the API and reads the byte back at offset 119. Verified in both
directions -- PASSED with this change, and with only the three source files
reverted it reports

    FAIL FSCLAS ... reaches the file: got ' ', want 'U'

Co-Authored-By: Claude Opus 5 <noreply@anthropic.com>

src/imaging/ossimNitf20Writer.cpp
@@ -130,6 +130,14 @@ bool ossimNitf20Writer::writeFile()
 
    // Write out the geometry info.
    writeGeometry(theImageHeader.get(), theInputConnection.get());
+
+   //---
+   // Apply anything set through setFileHeaderProperty() and
+   // setImageHeaderProperty().  Must happen before the headers are
+   // serialised.
+   //---
+   addFileHeaderProperties( theFileHeader.get() );
+   addImageHeaderProperties( theImageHeader.get() );
    
    addTags();
    

src/imaging/ossimNitfWriter.cpp
@@ -163,6 +163,14 @@ bool ossimNitfWriter::writeFile()
    // Write out the geometry info.
    writeGeometry(m_imageHeader.get(), theInputConnection.get());
 
+   //---
+   // Apply anything set through setFileHeaderProperty() and
+   // setImageHeaderProperty().  Must happen before the headers are
+   // serialised.
+   //---
+   addFileHeaderProperties( m_fileHeader.get() );
+   addImageHeaderProperties( m_imageHeader.get() );
+
    // addStandardTags();
    
    bool result = false;

src/imaging/ossimNitfWriterBase.cpp
@@ -265,17 +265,6 @@ void ossimNitfWriterBase::writeGeometry(ossimNitfImageHeaderV2_X* hdr,
          }
       }
 
-#if 0 /* Moved to addImageHeaderProperties */
-      if ( m_imageHeaderProps.size() )
-      {
-         const auto& cv = m_imageHeaderProps;
-         for ( auto&& i : cv )
-         {
-            hdr->setProperty( i );
-         }
-      }
-#endif
-      
    } // matches: if (hdr && seq)
 }
 

test/src/imaging/CMakeLists.txt
@@ -27,3 +27,4 @@ OSSIM_SETUP_APPLICATION(ossim-fft-test INSTALL COMMAND_LINE COMPONENT_NAME ossim
 OSSIM_SETUP_APPLICATION(ossim-feather-mosaic-test INSTALL COMMAND_LINE COMPONENT_NAME ossim SOURCE_FILES ossim-feather-mosaic-test.cpp)
 
 OSSIM_SETUP_APPLICATION(ossim-image-handler-state-test INSTALL COMMAND_LINE COMPONENT_NAME ossim SOURCE_FILES ossim-image-handler-state-test.cpp)
+OSSIM_SETUP_APPLICATION(ossim-nitf-writer-property-test INSTALL COMMAND_LINE COMPONENT_NAME ossim SOURCE_FILES ossim-nitf-writer-property-test.cpp)

test/src/imaging/ossim-nitf-writer-property-test.cpp
@@ -1,0 +1,114 @@
+//----------------------------------------------------------------------------
+//
+// License:  MIT
+//
+// See LICENSE.txt file in the top level directory for more details.
+//
+// Description: Regression test for setFileHeaderProperty() /
+// setImageHeaderProperty() reaching the written NITF.
+//
+// Both setters push onto vectors whose only consumers are
+// addFileHeaderProperties() / addImageHeaderProperties().  Between
+// a694e18f (2025-06-13) and this test, NOTHING called either method -- the
+// image-header call site was disabled with "#if 0 /* Moved to
+// addImageHeaderProperties */" and the new methods were never wired to a
+// caller, and the file-header half never had a call site at all.  So the
+// public API silently discarded everything set through it: FSCLAS came out a
+// space no matter what the caller asked for.
+//
+// This writes a small NITF with FSCLAS/ISCLAS set through that API and reads
+// the bytes back out of the file.  It fails on the unfixed code.
+//
+//----------------------------------------------------------------------------
+
+#include <ossim/base/ossimFilename.h>
+#include <ossim/init/ossimInit.h>
+#include <ossim/base/ossimIrect.h>
+#include <ossim/base/ossimRefPtr.h>
+#include <ossim/base/ossimStringProperty.h>
+#include <ossim/imaging/ossimImageData.h>
+#include <ossim/imaging/ossimImageDataFactory.h>
+#include <ossim/imaging/ossimMemoryImageSource.h>
+#include <ossim/imaging/ossimNitfWriter.h>
+
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+namespace
+{
+   int failures = 0;
+
+   void check(bool ok, const std::string& what)
+   {
+      std::cout << (ok ? "  ok   " : "  FAIL ") << what << "\n";
+      if (!ok)
+      {
+         ++failures;
+      }
+   }
+
+   // FSCLAS is a single byte at offset 119 of a NITF 2.1 file header:
+   // FHDR(4) FVER(5) CLEVEL(2) STYPE(4) OSTAID(10) FDT(14) FTITLE(80) = 119.
+   const std::streamoff FSCLAS_OFFSET = 119;
+
+   std::string byteAt(const ossimFilename& file, std::streamoff offset)
+   {
+      std::ifstream in(file.c_str(), std::ios::binary);
+      if (!in)
+      {
+         return std::string();
+      }
+      in.seekg(offset);
+      char c = '\0';
+      in.get(c);
+      return in ? std::string(1, c) : std::string();
+   }
+}
+
+int main(int argc, char* argv[])
+{
+   ossimInit::instance()->initialize(argc, argv);
+
+   const ossimFilename output("ossim-nitf-writer-property-test.ntf");
+   output.remove();
+
+   // A small, uniform source image is enough; this test is about the header.
+   ossimIrect rect(0, 0, 63, 63);
+   ossimRefPtr<ossimImageData> tile =
+      ossimImageDataFactory::instance()->create(0, OSSIM_UINT8, 1, 64, 64);
+   tile->initialize();
+   tile->fill(128.0);
+   tile->setImageRectangle(rect);
+
+   ossimRefPtr<ossimMemoryImageSource> source = new ossimMemoryImageSource();
+   source->setImage(tile);
+
+   ossimRefPtr<ossimNitfWriter> writer = new ossimNitfWriter();
+   writer->connectMyInputTo(0, source.get());
+   writer->setFilename(output);
+
+   // THE API UNDER TEST.
+   writer->setFileHeaderProperty(
+      new ossimStringProperty(ossimString("FSCLAS"), ossimString("U")));
+   writer->setImageHeaderProperty(
+      new ossimStringProperty(ossimString("ISCLAS"), ossimString("U")));
+
+   const bool wrote = writer->execute();
+   check(wrote, "the writer produced a file");
+   if (!wrote || !output.exists())
+   {
+      std::cout << "\nFAILED\n";
+      return 1;
+   }
+
+   const std::string fsclas = byteAt(output, FSCLAS_OFFSET);
+   check(fsclas == "U",
+         "FSCLAS set through setFileHeaderProperty() reaches the file: got '" +
+         fsclas + "', want 'U'");
+
+   output.remove();
+   std::cout << "\n" << (failures ? "FAILED" : "PASSED") << "\n";
+   return failures ? 1 : 0;
+}